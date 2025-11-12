7607041011525f25c8aaccd8db976810409e9412
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-11-12 21:11:48 +0000

    new commit

--- utils/credential_check.py
+++ utils/credential_check.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
-from schema.schema import user_credentials
+from schema.schema import user_credentials 
 from pymongo import AsyncMongoClient
 import asyncio, os
 from dotenv import load_dotenv
@@ -9,22 +10,31 @@ load_dotenv()
 
 URI = os.getenv("MONGODB")
 
-mongo_db = AsyncMongoClient(URI)
-
-async def get_databases():
-    collections = await mongo_db.list_database_names()
-    for collection in collections:
-        print(collection)
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    mongo_db = AsyncMongoClient(URI)
+    app.state.mongo_client = mongo_db
+    app.state.db = mongo_db["nesthost_db"]
+    app.state.collections = app.state.db["users"]
+    yield
     
-print(asyncio.run(get_databases()))
-# form_app = FastAPI(title="form_app")
-
-# @form_app.post("/register")
-# async def register_credentials(cred:user_credentials):
-#     name = cred.username
-#     passw = cred.password
+    await mongo_db.close()
+    print("application shut donw successfully")
     
 
+
+form_app = FastAPI(lifespan=lifespan)
+
+@form_app.post("/register")
+async def register_credentials(cred:user_credentials, request:Request):
+    collection = request.app.state.collections
+    return collection
+        
+
+    
+    
+    
+    
 
 # @form_app.post("/login")
 # async def check_credentials(cred:user_credentials):
