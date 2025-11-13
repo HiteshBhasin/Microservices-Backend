dec1aab17c7a370cffb174e5250c1ac0882cb6f7
Hitesh Bhasin <bhasinsukh@gmail.com>
2025-11-13 15:32:04 -0600

    new commit

--- utils/credential_check.py
+++ utils/credential_check.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from schema.schema import UserCredentials
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import AsyncMongoClient
 from dotenv import load_dotenv
 import os, logging, hashlib
 
@@ -11,12 +11,12 @@ URI = os.getenv("MONGODB")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    mongo_client = AsyncIOMotorClient(URI)
+    mongo_client = AsyncMongoClient(URI)
     app.state.mongo_client = mongo_client
     app.state.db = mongo_client["nesthost_db"]
     app.state.collections = app.state.db["users"]
     yield
-    mongo_client.close()
+    await mongo_client.close()
     print("Application shut down successfully")
 
 form_app = FastAPI(lifespan=lifespan)
@@ -46,6 +46,17 @@ async def register_credentials(cred: UserCredentials, request: Request):
     result = await collection.insert_one(new_user)
     return {"message": "User registered successfully!", "user_id": str(result.inserted_id)}
 
+@form_app.post("/login")
+async def login_page(cred:UserCredentials, request:Request):
+    collection = request.app.state.collections
+    user = await collection.find_one({"username":cred.username})
+    if not user:
+        return f"Username or password are not correct{cred.username}or {cred.password}"
+    else:
+        
+        
+    
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("credential_check:form_app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)), reload=True)
