9da31c23f61a3a136da45c22a1c3fdce9e2bc4dd
Hitesh Bhasin <bhasinsukh@gmail.com>
2025-11-13 14:59:27 -0600

    new commit

--- ai_agent/my_agent/agent.py
+++ ai_agent/my_agent/agent.py
@@ -1,4 +1,8 @@
 from google.adk.agents.llm_agent import Agent
+import asyncio
+from google.adk.models.lite_llm import LiteLlm
+from mcp.client.stdio import stdio_client
+from mcp import ClientSession, StdioServerParameters
 
 root_agent = Agent(
     model='gemini-2.5-flash',
@@ -6,3 +10,6 @@ root_agent = Agent(
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
 )
+
+def get_mcp_sessions():
+    
