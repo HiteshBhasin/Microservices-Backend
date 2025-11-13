2c6d779c0e2ec740c3819d206ebe7aba1d941e53
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-11-13 14:08:25 -0600

    new commit

--- utils/credential_check.py
+++ utils/credential_check.py
@@ -1,32 +1,26 @@
 from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
-from schema.schema import UserCredentials
-from pymongo import AsyncMongoClient
-import asyncio, os
+from schema import UserCredentials
+from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
-import logging
+import os, logging, hashlib
 
 load_dotenv()
-
 URI = os.getenv("MONGODB")
 
 @asynccontextmanager
-async def lifespan(app:FastAPI):
-    mongo_db = AsyncMongoClient(URI)
-    app.state.mongo_client = mongo_db
-    app.state.db = mongo_db["nesthost_db"]
+async def lifespan(app: FastAPI):
+    mongo_client = AsyncIOMotorClient(URI)
+    app.state.mongo_client = mongo_client
+    app.state.db = mongo_client["nesthost_db"]
     app.state.collections = app.state.db["users"]
     yield
-    
-    await mongo_db.close()
-    print("application shut down successfully")
-    
-
+    mongo_client.close()
+    print("Application shut down successfully")
 
 form_app = FastAPI(lifespan=lifespan)
 
-# Add CORS middleware
 form_app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -35,20 +29,23 @@ form_app.add_middleware(
     allow_headers=["*"],
 )
 
+@form_app.get("/")
+async def index():
+    return {"message": "hi"}
+
 @form_app.post("/register")
 async def register_credentials(cred: UserCredentials, request: Request):
+    hash_obj = hashlib.sha256(cred.password.encode("utf-8"))
+    encoded_obj = hash_obj.hexdigest()
     collection = request.app.state.collections
-    # Convert Pydantic model to dict for MongoDB
-    user_dict = cred.model_dump()
-    result = await collection.insert_one(user_dict)
+    new_user = {
+        "emai":cred.email,
+        "username":cred.username,
+        "password": encoded_obj
+    }
+    result = await collection.insert_one(new_user)
     return {"message": "User registered successfully!", "user_id": str(result.inserted_id)}
 
-
-
 if __name__ == "__main__":
-    try:
-        import uvicorn
-        uvicorn.run("credential_check:form_app", host="0.0.0.0", port=int(os.getenv("PORT", 3000)), reload=True)
-    except Exception as exc:
-        import logging
-        logging.error("Failed to start uvicorn: %s", exc)
+    import uvicorn
+    uvicorn.run("credential_check:form_app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)), reload=True)
